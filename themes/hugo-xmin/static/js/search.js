e353d2f97fb7e33ead2c3c67fc16647c1d428857
KemoNine <mcrosson@kemonine.info>
2024-09-02 12:58:14 -0400

    cleanup / fix search

--- themes/hugo-xmin/static/js/search.js
+++ themes/hugo-xmin/static/js/search.js
@@ -1,136 +1,134 @@
-var summaryInclude = 180;
-var fuseOptions = {
-    shouldSort: true,
-    includeMatches: true,
-    includeScore: true,
-    tokenize: true,
-    location: 0,
-    distance: 100,
-    minMatchCharLength: 1,
-    keys: [
-        {name: "title", weight: 0.45},
-        {name: "contents", weight: 0.4},
-        {name: "tags", weight: 0.1},
-        {name: "categories", weight: 0.05}
-    ]
+// @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
+// How many characters to include on either side of match keyword
+const summaryInclude=60;
+
+// Options for fuse.js
+let fuseOptions = {
+  shouldSort: true,
+  includeMatches: true,
+  tokenize: true,
+  matchAllTokens: true,
+  threshold: 0.0,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 64,
+  minMatchCharLength: 3,
+  keys: [
+    {name:"title",weight:0.8},
+    {name:"tags",weight:0.5},
+    {name:"categories",weight:0.5},
+    {name:"contents",weight:0.4}
+  ]
 };
 
-// =============================
-// Search
-// =============================
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  let regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  let results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
 
-var inputBox = document.getElementById('search-query');
-if (inputBox !== null) {
-    var searchQuery = param("q");
-    if (searchQuery) {
-        inputBox.value = searchQuery || "";
-        executeSearch(searchQuery, false);
-    } else {
-        document.getElementById('search-results').innerHTML = '<p class="search-results-empty">Please enter a word or phrase above to search</p>';
-    }
+let searchQuery = getUrlParameter('q');
+
+if(searchQuery){
+  document.getElementById("search-query").value = searchQuery;
+  executeSearch(searchQuery);
+} else {
+  document.getElementById('search-results').innerHTML = "<p class=\"no-results\">Please enter a word or phrase above</p>";
 }
 
 function executeSearch(searchQuery) {
-
-    show(document.querySelector('.search-loading'));
-
-    fetch('/index.json').then(function (response) {
-        if (response.status !== 200) {
-            console.log('Looks like there was a problem. Status Code: ' + response.status);
-            return;
-        }
-        // Examine the text in the response
-        response.json().then(function (pages) {
-            var fuse = new Fuse(pages, fuseOptions);
-            var result = fuse.search(searchQuery);
-            if (result.length > 0) {
-                populateResults(result);
-            } else {
-                document.getElementById('search-results').innerHTML = '<p class=\"search-results-empty\">No matches found</p>';
-            }
-            hide(document.querySelector('.search-loading'));
-        })
-        .catch(function (err) {
-            console.log('Fetch Error :-S', err);
-        });
-    });
-}
-
-function populateResults(results) {
-
-    var searchQuery = document.getElementById("search-query").value;
-    var searchResults = document.getElementById("search-results");
-
-    // pull template from hugo template definition
-    var templateDefinition = document.getElementById("search-result-template").innerHTML;
-
-    results.forEach(function (value, key) {
-
-        var contents = value.item.contents;
-        var snippet = "";
-        var snippetHighlights = [];
-
-        snippetHighlights.push(searchQuery);
-        snippet = contents.substring(0, summaryInclude * 2) + '&hellip;';
-
-        //replace values
-        var tags = ""
-        if (value.item.tags) {
-            value.item.tags.forEach(function (element) {
-                tags = tags + "<a href='/tags/" + element + "'>" + "#" + element + "</a> "
-            });
-        }
-
-        var output = render(templateDefinition, {
-            key: key,
-            title: value.item.title,
-            link: value.item.permalink,
-            tags: tags,
-            categories: value.item.categories,
-            snippet: snippet
-        });
-        searchResults.innerHTML += output;
-
-        snippetHighlights.forEach(function (snipvalue, snipkey) {
-            var instance = new Mark(document.getElementById('summary-' + key));
-            instance.mark(snipvalue);
-        });
-
-    });
-}
-
-function render(templateString, data) {
-    var conditionalMatches, conditionalPattern, copy;
-    conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
-    //since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
-    copy = templateString;
-    while ((conditionalMatches = conditionalPattern.exec(templateString)) !== null) {
-        if (data[conditionalMatches[1]]) {
-            //valid key, remove conditionals, leave contents.
-            copy = copy.replace(conditionalMatches[0], conditionalMatches[2]);
-        } else {
-            //not valid, remove entire section
-            copy = copy.replace(conditionalMatches[0], '');
-        }
+  // Look for "index.json" in the same directory where this script is called.
+  fetch("/index.json").
+  then(function (response) {
+    return response.json()
+  }).
+  then(function (data) {
+    let fuse = new Fuse(data, fuseOptions);
+    let result = fuse.search(searchQuery);
+    if (result.length > 0) {
+      populateResults(result);
+    } else {
+      document.getElementById('search-results').innerHTML = "<p class=\"no-results\">No matches found</p>";
     }
-    templateString = copy;
-    //now any conditionals removed we can do simple substitution
-    var key, find, re;
-    for (key in data) {
-        find = '\\$\\{\\s*' + key + '\\s*\\}';
-        re = new RegExp(find, 'g');
-        templateString = templateString.replace(re, data[key]);
-    }
-    return templateString;
+  });
 }
 
-// Helper Functions
-function show(elem) {
-    elem.style.display = 'block';
+function populateResults(result){
+  result.forEach( function (value, key) {
+    let contents= value.item.contents;
+    let snippet = "";
+    let snippetHighlights=[];
+    snippetHighlights.push(searchQuery);
+    if(snippet.length<1){
+      var getSentenceByWordRegex = new RegExp( 
+        `[^.?!]*(?<=[.?\\s!])${searchQuery}(?=[\\s.?!])[^.?!]*[.?!]`,
+        'i'
+      );
+      var maxTextLength = summaryInclude*2
+      // Index of the matched search term
+      var indexOfMatch = contents.toLowerCase().indexOf(
+        searchQuery.toLowerCase()
+      );
+      // Index of the first word of the sentence with the search term in it
+      var indexOfSentence = contents.indexOf(
+        getSentenceByWordRegex.exec(contents)
+      );
+      
+      var start 
+      var cutStart = false
+      // Is the match in the result?
+      if(indexOfSentence+maxTextLength < indexOfMatch){
+        // Make sure that the match is in the result
+        start = indexOfMatch
+        // This bool is used to replace the first part with '...'
+        cutStart = true 
+      } else {
+        // Match is in view, even if we show the whole sentence
+        start = indexOfSentence
+      }
+      
+      // Change end length to the text length if it is longer than 
+      // the text length to prevent problems
+      var end = start + maxTextLength 
+      if (end > contents.length){
+        end = contents.length
+      }
+
+      if(cutStart){
+        // Replace first three characters with '...'
+        end -= 3;
+        snippet += "…" + contents.substring(start, end).trim();
+      }
+      else{
+        snippet += contents.substring(start, end).trim();
+      }     
+    }
+    snippet += "…";
+
+    // Lifted from https://stackoverflow.com/posts/3700369/revisions
+    var elem = document.createElement('textarea');
+    elem.innerHTML = snippet;
+    var decoded = elem.value;
+
+    // Pull template from hugo template definition
+    let frag = document.getElementById('search-result-template').content.cloneNode(true);
+    // Replace values
+    frag.querySelector(".search_summary").setAttribute("id", "summary-" + key);
+    frag.querySelector(".search_link").setAttribute("href", value.item.permalink);
+    frag.querySelector(".search_title").textContent = value.item.title;
+    frag.querySelector(".search_snippet").textContent = decoded;
+    let tags = value.item.tags;
+    if (tags) {
+      frag.querySelector(".search_tags").textContent = tags;
+    } else {
+      frag.querySelector(".search_iftags").remove();
+    }
+    snippetHighlights.forEach( function (snipvalue, snipkey) {
+      let markjs = new Mark(frag);
+      markjs.mark(snipvalue);
+    });
+    document.getElementById("search-results").appendChild(frag);
+  });
 }
-function hide(elem) {
-    elem.style.display = 'none';
-}
-function param(name) {
-    return decodeURIComponent((location.search.split(name + '=')[1] || '').split('&')[0]).replace(/\+/g, ' ');
-}
+// @license-end
